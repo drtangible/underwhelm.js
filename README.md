1529d0f6eb59de699d8e772c7fe87b8a4834ca1c
Dan Gilbert <danielcgilbert@gmail.com>
2013-03-23 18:25:39 -0500

    Adding ___.maybeMap.

--- README.md
+++ README.md
@@ -26,3 +26,23 @@ Return either `true` or `false`.
 
     ___.coinflip();
     => false
+
+
+### ___.maybeMap(list, transformation)
+
+Produce a new array of values by taking each value in a list and (maybe) applying a transformation function.
+
+    ___.maybeMap([1, 2, 3, 4, 5], function(value) {
+      return value + 10;
+    });
+    => [11, 2, 13, 14, 5]
+
+    ___.maybeMap([1, 2, 3, 4, 5], function(value) {
+      return value + 10;
+    });
+    => [11, 12, 3, 4, 5]
+
+    ___.maybeMap([1, 2, 3, 4, 5], function(value) {
+      return value + 10;
+    });
+    => [11, 2, 13, 4, 15]
--- underwhelm.js
+++ underwhelm.js
@@ -8,15 +8,30 @@
     return list[randomIndex];
   };
 
+
   // Return either `true` or `false`.
   var coinflip = function() {
     return random([true, false]);
   };
 
+
+  // Produce a new array of values by taking each value in a list and (maybe) applying a transformation function.
+  var maybeMap = function(list, transformation) {
+    var possiblyTransformedValues = [];
+
+    for (var i=0; i<list.length; i++) {
+      possiblyTransformedValues.push(coinflip() ? list[i] : transformation(list[i]))
+    };
+
+    return possiblyTransformedValues;
+  };
+
+
   // Assert dominance with three underscores.
   root.___ = {
     random: random,
-    coinflip: coinflip
+    coinflip: coinflip,
+    maybeMap: maybeMap
   };
 
 }).call(this);
